7c825e8e14e9db6c4c9759622601a5e750d89097
Made Arya Astawa <90488518+AryaDewata@users.noreply.github.com>
2024-02-05 09:07:00 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function backgroundNavbar() {
   }
 }
 
-const scrollElements = document.querySelectorAll(".js-scroll");
+const scrollElements = document.querySelectorAll(".scroll-element");
 const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
   return elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend;
